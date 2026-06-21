0d5f3d2e796388397873548f396b8f2ac5155894
Hitesh <nxtwave@Nxtwaves-MacBook-Pro.local>
2026-06-21 23:33:28 +0530

    .

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,41 +1,20 @@
 from dotenv import load_dotenv
 import os
-
-
 import streamlit as st
 
 load_dotenv()
-OPENAI_API_KEY = (
-    st.secrets.get("OPENAI_API_KEY")
-    or os.getenv("OPENAI_API_KEY")
+
+OPENAI_API_KEY = st.secrets.get(
+    "OPENAI_API_KEY",
+    os.getenv("OPENAI_API_KEY")
 )
 
-GOOGLE_SHEET_ID = (
-    st.secrets.get("GOOGLE_SHEET_ID")
-    or os.getenv("GOOGLE_SHEET_ID")
+GOOGLE_SHEET_ID = st.secrets.get(
+    "GOOGLE_SHEET_ID",
+    os.getenv("GOOGLE_SHEET_ID")
 )
 
-MEM0_API_KEY = (
-    st.secrets.get("MEM0_API_KEY")
-    or os.getenv("MEM0_API_KEY")
-)
-
-CHAT_MODEL = os.getenv(
-    "CHAT_MODEL",
-    "gpt-5.4-mini-2026-03-17"
-)
-
-EMBEDDING_MODEL = os.getenv(
-    "EMBEDDING_MODEL",
-    "text-embedding-3-small"
-)
-
-CHROMA_PATH = os.getenv(
-    "CHROMA_PATH",
-    "chroma_db"
-)
-
-COLLECTION_NAME = os.getenv(
-    "COLLECTION_NAME",
-    "student_kb"
+MEM0_API_KEY = st.secrets.get(
+    "MEM0_API_KEY",
+    os.getenv("MEM0_API_KEY")
 )
--- src/llm/openai_client.py
+++ src/llm/openai_client.py
@@ -1,24 +1,8 @@
-from langchain_openai import ChatOpenAI
-
-import os
 import streamlit as st
 
-from src.config.settings import (
-    CHAT_MODEL
-)
+print("SECRETS KEYS:", list(st.secrets.keys()))
 
-OPENAI_API_KEY = (
-    st.secrets.get("OPENAI_API_KEY")
-    or os.getenv("OPENAI_API_KEY")
-)
+OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY")
 
-if not OPENAI_API_KEY:
-    raise ValueError(
-        "OPENAI_API_KEY not found in Streamlit Secrets or environment variables"
-    )
-
-llm = ChatOpenAI(
-    model=CHAT_MODEL,
-    api_key=OPENAI_API_KEY,
-    temperature=0.3
-)
+print("OPENAI FOUND:", OPENAI_API_KEY is not None)
+print("OPENAI LENGTH:", len(OPENAI_API_KEY) if OPENAI_API_KEY else 0)
